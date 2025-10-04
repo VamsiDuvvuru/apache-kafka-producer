741507192f8adb363ca6b1e04aaea2bcb4d40231
vamsi <vamsiduvvuru999@gmail.com>
2025-10-04 12:37:25 +0530

    Add dependency for Kafka consumer core and update import for ProductCreateEvent

--- src/main/groovy/com/learnp/kafka/demo/CreateProductRequest.java
+++ src/main/groovy/com/learnp/kafka/demo/CreateProductRequest.java
@@ -1,7 +1,10 @@
 package com.learnp.kafka.demo;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Data
 public class CreateProductRequest {
 
     private String title;
@@ -9,28 +12,4 @@ public class CreateProductRequest {
     private BigDecimal price;
 
     private Integer quantity;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
